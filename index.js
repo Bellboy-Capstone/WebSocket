534866aacf9c835e8a9649260c74b122046c3b6b
Elma Khandaker <45903965+elmakhandaker@users.noreply.github.com>
2020-11-20 20:17:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,13 @@ var WebSocket = require("ws");
 var webSocketServer = require("websocket").server;
 var webSocketsServerPort = process.env.PORT || 3000;
 
-var clients = []; //list of connected clients
-
-//helps wtih input strings
-function htmlEntities(str) {
-  return String(str)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
-}
-
 //http server
 var server = http.createServer(function (request, response) {
-  var homePageHTML = fs.readFileSync("./index.html");
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(homePageHTML);
-  response.end();
+    console.log((new Date()) + ' Received request for ' + request.url)
+    var homePageHTML = fs.readFileSync("./index.html");
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(homePageHTML);
+    response.end();
 });
 
 //websocket server
@@ -31,28 +21,33 @@ server.listen(webSocketsServerPort, function () {
 });
 
 //websocket server request
-var wsServer = new webSocketServer({
+var bellboy = new webSocketServer({
   httpServer: server,
 });
 
 //gets called everytime a connection occurs with WebSocket
-wsServer.on("request", function (request) {
+bellboy.on("request", function (request) {
   console.log(new Date() + " Connection from origin " + request.origin + ".");
-  var connection = request.accept(null, request.origin);
-  var index = clients.push(connection) - 1;
+  var connection = request.accept('echo-protocol', request.origin);
   var client_name = true;
   console.log(" Connection accepted.");
 
   connection.on("message", function (message) {
+    if (message.type === 'utf8') {
+        console.log('Received Message: ' + message.utf8Data);
+        connection.sendUTF(message.utf8Data);
+    }
+    else if (message.type === 'binary') {
+        console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+        connection.sendBytes(message.binaryData);
+    }
+})
     const readline = require("readline").createInterface({
-      input: process.stdin,
-      output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
     });
+
     readline.question("Hello, who is this?", (message) => {
-      if (message.type === "utf8") {
-        if (client_name === true) {
-          client_name = htmlEntities(message.utf8Data);
-        } else {
           // log and broadcast the message
           console.log(
             new Date() +
@@ -60,16 +55,14 @@ wsServer.on("request", function (request) {
               message.utf8Data +
               ",you are connected to BellBoy deivce"
           );
-        }
-      }
-    });
+        });
 
     connection.on("close", function (connection) {
       if (client_name !== true) {
         console.log(" Peer " + connection.remoteAddress + " disconnected.");
         // remove client from the list of connected clients
-        clients.splice(index, 1);
+    
       }
     });
   });
-});
+
