c6cef1a58179ad6b3af37bb02f6c643ec11dd7dd
Elma Khandaker <45903965+elmakhandaker@users.noreply.github.com>
2020-11-24 21:43:42 -0500

    Update index.js
    Removed all unnecessary code and created a simple web socket to send message to client.

--- index.js
+++ index.js
@@ -13,56 +13,32 @@ var server = http.createServer(function (request, response) {
     response.end();
 });
 
+//websocket server request
+var bellboy = new webSocketServer({
+  httpServer: server,
+});
+
+//when bellboy is connected
+bellboy.on('connection',function connection(ws){
+  // send message to all clients
+    ws.on("message", function incoming(message) {
+      bellboy.clients.forEach(function each(client) {
+          if (client !== ws && client.readyState === WebSocket.OPEN ) {
+            client.send(message);
+          }
+        });
+      })
+    // close connection
+    ws.on("close", function (connection) {
+        console.log(" Peer " + connection.remoteAddress + " disconnected.");
+ 
+    })
+
+  });
+
 //websocket server
 server.listen(webSocketsServerPort, function () {
   console.log(
     new Date() + " Server is listening on port " + webSocketsServerPort
   );
 });
-
-//websocket server request
-var bellboy = new webSocketServer({
-  httpServer: server,
-});
-
-//gets called everytime a connection occurs with WebSocket
-bellboy.on("request", function (request) {
-  console.log(new Date() + " Connection from origin " + request.origin + ".");
-  var connection = request.accept('echo-protocol', request.origin);
-  var client_name = true;
-  console.log(" Connection accepted.");
-
-  connection.on("message", function (message) {
-    if (message.type === 'utf8') {
-        console.log('Received Message: ' + message.utf8Data);
-        connection.sendUTF(message.utf8Data);
-    }
-    else if (message.type === 'binary') {
-        console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-        connection.sendBytes(message.binaryData);
-    }
-})
-    const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    });
-
-    readline.question("Hello, who is this?", (message) => {
-          // log and broadcast the message
-          console.log(
-            new Date() +
-              " Hello !" +
-              message.utf8Data +
-              ",you are connected to BellBoy deivce"
-          );
-        });
-
-    connection.on("close", function (connection) {
-      if (client_name !== true) {
-        console.log(" Peer " + connection.remoteAddress + " disconnected.");
-        // remove client from the list of connected clients
-    
-      }
-    });
-  });
-
